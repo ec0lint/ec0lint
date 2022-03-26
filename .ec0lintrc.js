e4674e2eae52358028980f22a5e96ed2b80903f7
malwinq <malwina.kubas@gmail.com>
2022-03-26 18:55:42 +0100

    remove unused ec0lintrc configs

--- .ec0lintrc.js
+++ .ec0lintrc.js
@@ -43,152 +43,26 @@ function createInternalFilesPatterns(pattern = null) {
 module.exports = {
     root: true,
     plugins: [
-        "eslint-plugin",
+        "ec0lint-plugin",
         "internal-rules"
     ],
     extends: [
-        "eslint:recommended",
-        "plugin:eslint-plugin/recommended"
+        "ec0lint:recommended",
+        "plugin:ec0lint-plugin/recommended"
     ],
     parserOptions: {
         ecmaVersion: 2021
     },
 
-    /*
-     * it fixes eslint-plugin-jsdoc's reports: "Invalid JSDoc tag name "template" jsdoc/check-tag-names"
-     * refs: https://github.com/gajus/eslint-plugin-jsdoc#check-tag-names
-     */
-    settings: {
-        jsdoc: {
-            mode: "typescript"
-        }
-    },
     rules: {
-        "eslint-plugin/prefer-message-ids": "error",
-        "eslint-plugin/prefer-output-null": "error",
-        "eslint-plugin/prefer-placeholders": "error",
-        "eslint-plugin/prefer-replace-text": "error",
-        "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
-        "eslint-plugin/require-meta-docs-description": "error",
-        "eslint-plugin/test-case-property-ordering": "error",
-        "eslint-plugin/test-case-shorthand-strings": "error",
-        "internal-rules/multiline-comment-style": "error"
+        "ec0lint-plugin/axios-calls": "error"
     },
     overrides: [
         {
             files: ["lib/rules/*", "tools/internal-rules/*"],
             excludedFiles: ["index.js"],
             rules: {
-                "internal-rules/no-invalid-meta": "error"
-            }
-        },
-        {
-            files: ["lib/rules/*"],
-            excludedFiles: ["index.js"],
-            rules: {
-                "eslint-plugin/require-meta-docs-url": ["error", { pattern: "https://eslint.org/docs/rules/{{name}}" }]
-            }
-        },
-        {
-            files: ["tests/**/*"],
-            env: { mocha: true },
-            rules: {
-                "no-restricted-syntax": ["error", {
-                    selector: "CallExpression[callee.object.name='assert'][callee.property.name='doesNotThrow']",
-                    message: "`assert.doesNotThrow()` should be replaced with a comment next to the code."
-                }]
-            }
-        },
-
-        // Restrict relative path imports
-        {
-            files: ["lib/*"],
-            excludedFiles: ["lib/unsupported-api.js"],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns()
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.CLI_ENGINE_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.CLI_ENGINE_PATTERN),
-                    resolveAbsolutePath("lib/init/index.js")
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.INIT_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.INIT_PATTERN),
-                    resolveAbsolutePath("lib/rule-tester/index.js")
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.LINTER_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.LINTER_PATTERN),
-                    "fs",
-                    resolveAbsolutePath("lib/cli-engine/index.js"),
-                    resolveAbsolutePath("lib/init/index.js"),
-                    resolveAbsolutePath("lib/rule-tester/index.js")
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.RULES_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.RULES_PATTERN),
-                    "fs",
-                    resolveAbsolutePath("lib/cli-engine/index.js"),
-                    resolveAbsolutePath("lib/init/index.js"),
-                    resolveAbsolutePath("lib/linter/index.js"),
-                    resolveAbsolutePath("lib/rule-tester/index.js"),
-                    resolveAbsolutePath("lib/source-code/index.js")
-                ]]
-            }
-        },
-        {
-            files: ["lib/shared/**/*"],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(),
-                    resolveAbsolutePath("lib/cli-engine/index.js"),
-                    resolveAbsolutePath("lib/init/index.js"),
-                    resolveAbsolutePath("lib/linter/index.js"),
-                    resolveAbsolutePath("lib/rule-tester/index.js"),
-                    resolveAbsolutePath("lib/source-code/index.js")
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.SOURCE_CODE_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.SOURCE_CODE_PATTERN),
-                    "fs",
-                    resolveAbsolutePath("lib/cli-engine/index.js"),
-                    resolveAbsolutePath("lib/init/index.js"),
-                    resolveAbsolutePath("lib/linter/index.js"),
-                    resolveAbsolutePath("lib/rule-tester/index.js"),
-                    resolveAbsolutePath("lib/rules/index.js")
-                ]]
-            }
-        },
-        {
-            files: [INTERNAL_FILES.RULE_TESTER_PATTERN],
-            rules: {
-                "node/no-restricted-require": ["error", [
-                    ...createInternalFilesPatterns(INTERNAL_FILES.RULE_TESTER_PATTERN),
-                    resolveAbsolutePath("lib/cli-engine/index.js"),
-                    resolveAbsolutePath("lib/init/index.js")
-                ]]
+                "ec0lint-plugin/axios-calls": "error"
             }
         }
     ]
