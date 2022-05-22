77102cc001bc0a87e6ad43ac7b7b303602a38eea
malwinq <malwina.kubas@gmail.com>
2022-05-22 14:10:21 +0200

    debug

--- tests/lib/cli-engine/cli-engine.js
+++ tests/lib/cli-engine/cli-engine.js
@@ -152,13 +152,13 @@ describe("CLIEngine", () => {
 
             assert.strictEqual(report.results.length, 1);
             assert.strictEqual(report.errorCount, 0);
-            assert.strictEqual(report.warningCount, 5);
-            assert.strictEqual(report.fixableErrorCount, 0);
-            assert.strictEqual(report.fixableWarningCount, 3);
             // assert.strictEqual(report.results[0].messages[0].ruleId, "strict");
             assert.strictEqual(report.results[0].messages[0].ruleId, "no-var");
+            assert.strictEqual(report.warningCount, 5);
             assert.strictEqual(report.results[0].messages[2].ruleId, "no-unused-vars");
+            assert.strictEqual(report.fixableErrorCount, 0);
             assert.strictEqual(report.results[0].messages[3].ruleId, "quotes");
+            assert.strictEqual(report.fixableWarningCount, 3);
             assert.strictEqual(report.results[0].messages[4].ruleId, "eol-last");
             assert.strictEqual(report.results[0].messages.length, 5);
             assert.strictEqual(report.results[0].fixableErrorCount, 0);
