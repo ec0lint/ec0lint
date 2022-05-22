b6046d8f418864c98ea0dfd4ef01a22608e747a6
malwinq <malwina.kubas@gmail.com>
2022-05-22 19:46:28 +0200

    remove ec0lint-plugin-ec0lint-plugin

--- .ec0lintrc.js
+++ .ec0lintrc.js
@@ -3,25 +3,15 @@
 module.exports = {
     root: true,
     plugins: [
-        "ec0lint-plugin",
         "internal-rules"
     ],
     extends: [
-        "ec0lint:recommended",
-        "plugin:ec0lint-plugin/recommended"
+        "ec0lint:recommended"
     ],
     parserOptions: {
         ecmaVersion: 2021
     },
     rules: {
-        "ec0lint-plugin/prefer-message-ids": "error",
-        "ec0lint-plugin/prefer-output-null": "error",
-        "ec0lint-plugin/prefer-placeholders": "error",
-        "ec0lint-plugin/prefer-replace-text": "error",
-        "ec0lint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
-        "ec0lint-plugin/require-meta-docs-description": "error",
-        "ec0lint-plugin/test-case-property-ordering": "error",
-        "ec0lint-plugin/test-case-shorthand-strings": "error",
         "internal-rules/multiline-comment-style": "error"
     },
     overrides: [
@@ -36,13 +26,6 @@ module.exports = {
             excludedFiles: ["index.js"],
             rules: {
                 "internal-rules/no-invalid-meta": "error"
-            }
-        },
-        {
-            files: ["lib/rules/*"],
-            excludedFiles: ["index.js"],
-            rules: {
-                "ec0lint-plugin/require-meta-docs-url": ["error", { pattern: "https://eslint.org/docs/rules/{{name}}" }]
             }
         },
         {
