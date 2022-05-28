bd64ae0becebfc7162e356add01e67cd6bc5f41b
malwinq <malwina.kubas@gmail.com>
2022-05-28 12:24:05 +0200

    clean

--- .ec0lintrc.js
+++ .ec0lintrc.js
@@ -12,29 +12,5 @@ module.exports = {
         ecmaVersion: 2021
     },
     rules: { },
-    overrides: [
-        {
-            files: ["tools/*.js"],
-            rules: {
-                "no-console": "off"
-            }
-        },
-        {
-            files: ["lib/rules/*", "tools/internal-rules/*"],
-            excludedFiles: ["index.js"],
-            rules: {
-                "internal-rules/no-invalid-meta": "error"
-            }
-        },
-        {
-            files: ["tests/**/*"],
-            env: { mocha: true },
-            rules: {
-                "no-restricted-syntax": ["error", {
-                    selector: "CallExpression[callee.object.name='assert'][callee.property.name='doesNotThrow']",
-                    message: "`assert.doesNotThrow()` should be replaced with a comment next to the code."
-                }]
-            }
-        }
-    ]
+    overrides: [ ]
 };
