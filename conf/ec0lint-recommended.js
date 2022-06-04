aa8a4dde44f74ae53742ac9abd80ccc0855e5302
Julia Ziębińska <julkazbs2000@gmail.com>
2022-06-04 17:22:25 +0200

    feat: remove no-f... rules

--- conf/ec0lint-recommended.js
+++ conf/ec0lint-recommended.js
@@ -12,8 +12,6 @@
 module.exports = {
     rules: {
         "lighter-http": "error",
-        "no-fallthrough": "error",
-        "no-func-assign": "error",
         "no-import-assign": "error",
         "no-inner-declarations": "error",
         "no-invalid-regexp": "error",
