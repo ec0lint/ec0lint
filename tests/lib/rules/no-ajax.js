c0eb64c3a6cdb8d683dce31101f325beb52237f0
Julia Ziębińska <julkazbs2000@gmail.com>
2022-08-13 17:21:44 +0200

    feat: add no-ajax rule. Part of no-jquery module

---
+++ tests/lib/rules/no-ajax.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const rule = require('../../../lib/rules/no-ajax'),
+    { RuleTester } = require("../../../lib/rule-tester");
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2022, sourceType: "module" } });
+
+const ajaxError = "You can replace most of the jQuery methods, for more go to https://youmightnotneedjquery.com/"
+
+
+
+ruleTester.run('no-ajax', rule, {
+    valid: [
+        'ajax()',
+        'div.ajax()',
+        'div.ajax',
+
+        'get()',
+        'div.get()',
+        'div.get',
+
+        'getJSON()',
+        'div.getJSON()',
+        'div.getJSON',
+
+        'getScript()',
+        'div.getScript()',
+        'div.getScript',
+
+        'post()',
+        'div.post()',
+        'div.post'
+    ],
+    invalid: [
+        {
+            code: '$.ajax()',
+            errors: [ajaxError]
+        },
+        {
+            code: '$.get()',
+            errors: [ajaxError]
+        },
+        {
+            code: '$.getJSON()',
+            errors: [ajaxError]
+        },
+        {
+            code: '$.getScript()',
+            errors: [ajaxError]
+        },
+        {
+            code: '$.post()',
+            errors: [ajaxError]
+        }
+    ]
+});
