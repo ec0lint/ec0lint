5cdb7136a2f946b6f75b1b7fb016ff134392c29f
Martyna Babiak <88138563+martinabab@users.noreply.github.com>
2023-02-22 12:35:20 +0100

    Tests update: lighter-image-files.js
    Move .gif to invalid
    Add .avif to valid

--- tests/lib/rules/lighter-image-files.js
+++ tests/lib/rules/lighter-image-files.js
@@ -7,23 +7,27 @@ const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2022, sourceTy
 
 ruleTester.run("lighter-image-files", rule, {
     valid: [
-        "import foo from './foo.gif'",
+        "import foo from './foo.avif'",
         "import foo from './foo.jpg'",
         "import foo from './foo.jpeg'",
         "import foo from './foo.svg'",
         "import foo from './foo.webp'",
-        "import {ReactComponent as foo} from './foo.gif'",
+        "import {ReactComponent as foo} from './foo.avif'",
         "import {ReactComponent as foo} from './foo.jpg'",
         "import {ReactComponent as foo} from './foo.jpeg'",
         "import {ReactComponent as foo} from './foo.svg'",
         "import {ReactComponent as foo} from './foo.webp'",
-        "let Foo = require('../src/foo.gif')",
+        "let Foo = require('../src/foo.avif')",
         "let Foo = require('../src/foo.jpg')",
         "let Foo = require('../src/foo.jpeg')",
         "let Foo = require('../src/foo.svg')",
         "let Foo = require('../src/foo.webp')"
     ],
     invalid: [
+        {
+            code: "import foo from './foo.gif'",
+            errors: [{ messageId: "rejected" }]
+        },
         {
             code: "import foo from './foo.png'",
             errors: [{ messageId: "rejected" }]
@@ -41,6 +45,10 @@ ruleTester.run("lighter-image-files", rule, {
             errors: [{ messageId: "rejected" }]
         },
         {
+            code: "import {ReactComponent as foo} from './foo.gif'",
+            errors: [{ messageId: "rejected" }]
+        },
+        {
             code: "import {ReactComponent as foo} from './foo.png'",
             errors: [{ messageId: "rejected" }]
         },
@@ -54,6 +62,10 @@ ruleTester.run("lighter-image-files", rule, {
         },
         {
             code: "import {ReactComponent as foo} from './foo.rgb'",
+            errors: [{ messageId: "rejected" }]
+        },
+        {
+            code: "let Foo = require('../src/foo.gif')",
             errors: [{ messageId: "rejected" }]
         },
         {
